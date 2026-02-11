e83d943c701a06fc1c56d6b52a0937601cf17a0a
Anujijitmini <anujijithmini8@gmail.com>
2026-02-11 16:49:58 +0530

    railway error

--- car_rental/backend/dist/index.js
+++ car_rental/backend/dist/index.js
@@ -1,40 +1,32 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
-const dotenv_1 = __importDefault(require("dotenv"));
-const db_1 = require("./config/db");
-dotenv_1.default.config();
-const app = (0, express_1.default)();
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { pool } from './config/db.js';
+import authRoutes from './routes/authRoutes.js';
+import vehicleRoutes from './routes/vehicleRoutes.js';
+dotenv.config();
+const app = express();
 const port = process.env.PORT || 3000;
-app.use((0, cors_1.default)());
-app.use(express_1.default.json());
+app.use(cors());
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+app.use('/api/auth', authRoutes);
+app.use('/api/vehicles', vehicleRoutes);
 app.get('/', (req, res) => {
     res.send('Car Rental API is running');
 });
 // Test DB connection
-app.get('/db-test', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.get('/db-test', async (req, res) => {
     try {
-        const result = yield db_1.pool.query('SELECT NOW()');
+        const result = await pool.query('SELECT NOW()');
         res.json(result.rows[0]);
     }
     catch (err) {
         console.error(err);
         res.status(500).json({ error: 'Database connection error', details: err.message });
     }
-}));
+});
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+//# sourceMappingURL=index.js.map
